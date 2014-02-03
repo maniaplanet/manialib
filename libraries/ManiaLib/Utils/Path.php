aed5824320a0b98f0381af785dd24b16f6c26e3b
Baptiste Lafontaine <baptiste33@gmail.com>
2014-02-03 15:25:18 +0100

    Improve Path support in Phar files

--- libraries/ManiaLib/Utils/Path.php
+++ libraries/ManiaLib/Utils/Path.php
@@ -13,26 +13,28 @@ class Path extends Singleton
 	public $log;
 	public $resources;
 	
-	public function getRoot()
+	public function getRoot($external = false)
 	{
+		if ($external && \Phar::running(false) != "")
+			return dirname(\Phar::running(false));
 		if (!$this->root && !defined(MANIALIB_APP_PATH))
 			throw new \Exception();
 		return $this->root ? : MANIALIB_APP_PATH;
 	}
 	
-	public function getConfig()
+	public function getConfig($external = false)
 	{
-		return $this->config ? : $this->getRoot().DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR;
+		return $this->config ? : $this->getRoot($external).DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR;
 	}
 	
-	public function getLog()
+	public function getLog($external = false)
 	{
-		return $this->log ? : $this->getRoot().DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR;
+		return $this->log ? : $this->getRoot($external).DIRECTORY_SEPARATOR.'logs'.DIRECTORY_SEPARATOR;
 	}
 	
-	public function getResources()
+	public function getResources($external = false)
 	{
-		return $this->resources ? : $this->getRoot().DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR;
+		return $this->resources ? : $this->getRoot($external).DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR;
 	}
 }
 ?>
